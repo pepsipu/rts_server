ccefa976b247a37c924d7359334258a3f517f7e4
Sammy Hajhamid <pokeso123@hotmail.com>
2020-01-12 21:12:59 -0800

    asyncore, and forwarding messages

--- main.py
+++ main.py
@@ -1,52 +1,84 @@
-import socket
-import threading
-import time
+import asyncore
 import json
+import socket
 
-PORT = 8000
+PORT = 8080
+ADDR = '0.0.0.0'
 
 matches = []
 lobby = []
 
 
-def handle_connection(conn, addr):
-    pass
+class MainServer(asyncore.dispatcher):
+    def __init__(self):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.bind((ADDR, PORT))
+        self.listen(20)
+        return
 
-
-def begin_match(p1, p2):
-    connection_message = json.dumps({
-        "info": "match_found"
-    }).encode()
-    p1["conn"].send(connection_message)
-    p2["conn"].send(connection_message)
-
-
-def match_players():
-    while True:
+    def handle_accept(self):
+        conn, addr = self.accept()
+        meta = {
+            "port": addr[1],
+            "ip": addr[0],
+            "conn": conn
+        }
+        print(meta)
+        lobby.append(meta)
         if len(lobby) > 1:
             print(f"matched {lobby[0]['ip']} with {lobby[1]['ip']}")
-            new_match = threading.Thread(target=begin_match, args=(lobby[0], lobby[1]))
-            matches.append(new_match)
-            new_match.start()
+            matches.append(Match(lobby[0], lobby[1]))
             del lobby[0]
             del lobby[0]
-        time.sleep(1)
+
+    def handle_close(self):
+        print("disconnect")
+
+
+class Player(asyncore.dispatcher):
+    opponent = None
+
+    def __init__(self, conn, addr):
+        self.addr = addr
+        self.buffer = b""
+        asyncore.dispatcher.__init__(self, sock=conn)
+
+    def handle_read(self):
+        chunk = self.recv(512)
+        self.buffer += chunk
+        if chunk.decode()[-1] == '\n':
+            cmd = json.loads(self.buffer.decode())
+
+            self.evaluate_cmd(cmd)
+
+    def evaluate_cmd(self, cmd):
+        if cmd["forward"]:
+            print(f"msg from {self.addr} to {self.opponent.addr}: {cmd['forward']}")
+            self.opponent.send(json.dumps(cmd["payload"]).encode())
+
+
+class Match:
+    def __init__(self, p1, p2):
+        connection_message = json.dumps({
+            "info": "match_found"
+        }).encode()
+        self.player1 = Player(p1["conn"], p1["ip"])
+        self.player2 = Player(p2["conn"], p2["ip"])
+        self.player1.opponent = self.player2
+        self.player2.opponent = self.player1
+        p1["conn"].send(connection_message)
+        p2["conn"].send(connection_message)
 
 
 def main():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('0.0.0.0', PORT))
-    sock.listen(20)
-    threading.Thread(target=match_players).start()
-    while True:
-        new_conn, new_addr = sock.accept()
-        new_player = {
-            "conn": new_conn,
-            "ip": new_addr[0],
-            "port": new_addr[1]
-        }
-        print(new_player)
-        lobby.append(new_player)  # add the IP to the lobby as well as a reference to the socket
+    server = MainServer()
+    try:
+        asyncore.loop()
+    except:
+        pass
+    print("server closing")
+    server.close()
 
 
 if __name__ == "__main__":
